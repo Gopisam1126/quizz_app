c6216464508bce53724a9e5166dd8cb26bd19c4b
scriptKid <gopikrishnan0045@gmail.com>
2025-01-10 12:51:51 +0530

    update anim - navabr

--- src/assets/components/navbar.jsx
+++ src/assets/components/navbar.jsx
@@ -1,16 +1,38 @@
+import gsap from "gsap";
+import { useEffect } from "react";
+
 function Navbar() {
+  const navItems = ["home", "Categories", "About", "Feedback"];
+
+  useEffect(() => {
+    gsap.to("#navbar", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+      duration: 1.5,
+    });
+  });
+
   return (
     <>
-      <nav className="flex justify-between items-center fixed top-8 left-20 right-20 px-10 py-2 bg-violet-600 shadow-lg rounded-lg">
-        <div className="">
-          <img src="images\logo.png" alt="logo" className="w-10 h-10 rounded-full" />
+      <nav
+        className="flex items-center fixed top-8 left-20 right-20 px-10 py-2 bg-violet-600 shadow-lg rounded-lg opacity-0 -translate-y-96"
+        id="navbar"
+      >
+        <div className="mr-auto">
+          <img
+            src="images\logo.png"
+            alt="logo"
+            className="w-10 h-10 rounded-full cursor-pointer"
+          />
         </div>
-        <ul className="flex gap-8 text-slate-100">
-          <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Categories</li>
-          <li className="cursor-pointer">About</li>
-          <li className="cursor-pointer">Feedback</li>
-        </ul>
+        <div className="flex gap-8 font-medium">
+          {navItems.map((item, i) => (
+            <a className="cursor-pointer nav-hover-btn" key={i}>
+              {item}
+            </a>
+          ))}
+        </div>
       </nav>
     </>
   );
