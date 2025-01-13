264382dfeb5956b1ed24b03b5040315eac631008
scriptKid <gopikrishnan0045@gmail.com>
2025-01-13 22:11:16 +0530

    update mq

--- src/assets/utils/mechQ.js
+++ src/assets/utils/mechQ.js
@@ -1,202 +1,202 @@
 export const mechQ = [
   {
     id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    category: "Mechanical",
+    question: "Which law states that energy cannot be created or destroyed?",
+    options: [
+      { option: 1, value: "Newtons first law" },
+      { option: 2, value: "First law of Thermodynamics" },
+      { option: 3, value: "Second law of Thermodynamics" },
+      { option: 4, value: "Boyle’s Law" },
+    ],
+    correct: 2,
+  },
+
+  {
+    id: 2,
+    category: "Mechanical",
+    question: "What is the SI unit of Pressure",
     options: [
       { option: 1, value: "Watt" },
       { option: 2, value: "N-m" },
       { option: 3, value: "Pascal" },
       { option: 4, value: "ohm" },
     ],
-    correct: 2,
+    correct: 3,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    id: 3,
+    category: "Mechanical",
+    question: "Which of these materials has the highest thermal conductivity?",
     options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
+      { option: 1, value: "Aluminum" },
+      { option: 2, value: "Copper" },
+      { option: 3, value: "Steel" },
+      { option: 4, value: "Plastic" },
     ],
     correct: 2,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    id: 4,
+    category: "Mechanical",
+    question: "What type of stress is developed in a body subjected to tensile load?",
     options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
+      { option: 1, value: "Compressive Stress" },
+      { option: 2, value: "Shear Stress" },
+      { option: 3, value: "Tensile Stress" },
+      { option: 4, value: "Bending Stress" },
+    ],
+    correct: 3,
+  },
+
+  {
+    id: 5,
+    category: "Mechanical",
+    question: "What is the function of a flywheel in an engine?",
+    options: [
+      { option: 1, value: "To increase speed" },
+      { option: 2, value: "To store energy" },
+      { option: 3, value: "To maintain constant speed" },
+      { option: 4, value: "To reduce friction" },
+    ],
+    correct: 3,
+  },
+
+  {
+    id: 6,
+    category: "Mechanical",
+    question: "In a four-stroke engine, how many power strokes occur in one cycle?",
+    options: [
+      { option: 1, value: "1" },
+      { option: 2, value: "2" },
+      { option: 3, value: "3" },
+      { option: 4, value: "4" },
+    ],
+    correct: 1,
+  },
+
+  {
+    id: 7,
+    category: "Mechanical",
+    question: "The bending moment at the fixed end of a cantilever beam is:",
+    options: [
+      { option: 1, value: "Maximum" },
+      { option: 2, value: "Minimum" },
+      { option: 3, value: "/zero" },
+      { option: 4, value: "Cannot be Determined" },
+    ],
+    correct: 1,
+  },
+
+  {
+    id: 8,
+    category: "Mechanical",
+    question: "What is the efficiency of an ideal Carnot engine operating between 600 K and 300 K?",
+    options: [
+      { option: 1, value: "33.3%" },
+      { option: 2, value: "50%" },
+      { option: 3, value: "66.7%" },
+      { option: 4, value: "75%" },
     ],
     correct: 2,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    id: 9,
+    category: "Mechanical",
+    question: "Which of the following processes is an example of adiabatic process?",
     options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
+      { option: 1, value: "Free expansion of gas" },
+      { option: 2, value: "Heating a gas in a closed container" },
+      { option: 3, value: "Expansion of gas in an insulated cylinder" },
+      { option: 4, value: "Boiling of water" },
+    ],
+    correct: 4,
+  },
+
+  {
+    id: 10,
+    category: "Mechanical",
+    question: "Which tool is used to measure angles with high precision?",
+    options: [
+      { option: 1, value: " Bevel protractor" },
+      { option: 2, value: "Vernier caliper" },
+      { option: 3, value: "Micrometer screw gauge" },
+      { option: 4, value: "Dial indicator" },
+    ],
+    correct: 1,
+  },
+
+  {
+    id: 11,
+    category: "Mechanical",
+    question: "Which type of bearing is used to support axial loads?",
+    options: [
+      { option: 1, value: "Ball bearing" },
+      { option: 2, value: "Roller bearing" },
+      { option: 3, value: "Thrust bearing" },
+      { option: 4, value: "Journal bearing" },
+    ],
+    correct: 3,
+  },
+
+  {
+    id: 12,
+    category: "Mechanical",
+    question: "The principle of conservation of mass is applied in which equation?",
+    options: [
+      { option: 1, value: "Bernoulli’s equation" },
+      { option: 2, value: "Continuity equation" },
+      { option: 3, value: "Navier-Stokes equation" },
+      { option: 4, value: "Euler’s equation" },
     ],
     correct: 2,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    id: 13,
+    category: "Mechanical",
+    question: "Which process is used to remove internal stresses in metal components?",
     options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
+      { option: 1, value: "Hardening" },
+      { option: 2, value: "Tempering" },
+      { option: 3, value: "Normalizing" },
+      { option: 4, value: "Annealing" },
     ],
-    correct: 2,
+    correct: 4,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    id: 14,
+    category: "Mechanical",
+    question: "Which type of welding is performed without melting the base metal?",
     options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
+      { option: 1, value: "Gas welding" },
+      { option: 2, value: "TIG welding" },
+      { option: 3, value: "Solid-state welding" },
+      { option: 4, value: "MIG welding" },
     ],
-    correct: 2,
+    correct: 3,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
+    id: 15,
+    category: "Mechanical",
+    question: "What is the purpose of fins in a heat exchanger?",
     options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
+      { option: 1, value: "Increase heat transfer area" },
+      { option: 2, value: "Increase pressure drop" },
+      { option: 3, value: "Reduce heat loss" },
+      { option: 4, value: "Improve insulation" },
     ],
-    correct: 2,
+    correct: 1,
   },
 
   {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
-    question: "What is the SI unit of torque",
-    options: [
-      { option: 1, value: "Watt" },
-      { option: 2, value: "N-m" },
-      { option: 3, value: "Pascal" },
-      { option: 4, value: "ohm" },
-    ],
-    correct: 2,
-  },
-
-  {
-    id: 1,
-    category: "mechanical",
+    id: 16,
+    category: "Mechanical",
     question: "What is the SI unit of torque",
     options: [
       { option: 1, value: "Watt" },
